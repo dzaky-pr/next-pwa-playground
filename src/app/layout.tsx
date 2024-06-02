831f3927b2ab84824f5b9e08aa085585e20706bb
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2024-06-02 11:25:25 +0700

    feat: setup PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 
 import Providers from '@/app/providers';
@@ -8,11 +8,16 @@ import Providers from '@/app/providers';
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
+  manifest: '/manifest.json',
   title: {
     default: 'Nextjs Starter Template',
     template: '%s | Nextjs Starter Template',
   },
   description: 'Nextjs 14.2.1 + Tailwind CSS starter template',
+};
+
+export const viewport: Viewport = {
+  themeColor: 'FFFFFF',
 };
 
 export default function RootLayout({
